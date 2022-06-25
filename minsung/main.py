b00a56321d710bffffae06c721aea41122127a54
mildstar <mildstar9436@gmail.com>
2022-06-25 10:43:05 +0900

    Studied EDA

--- minsung/main.py
+++ minsung/main.py
@@ -15,11 +15,11 @@ test2_df = pd.read_csv(dir_path +'/data/df/test2_df.csv')
 
 submission = pd.read_csv(dir_path +'/submit/submit.csv')
 
-train_X = train_df.drop(['date','heat_supply'], axis=1)
+train_X = train_df.drop(['Dates','Date','heat_supply'], axis=1)
 train_Y = train_df['heat_supply']
 
-test1_X = test1_df.drop(['date', 'heat_supply'],axis=1)
-test2_X = test2_df.drop(['date', 'heat_supply'],axis=1)
+test1_X = test1_df.drop(['Dates','Date','heat_supply'],axis=1)
+test2_X = test2_df.drop(['Dates','Date','heat_supply'],axis=1)
 
 
 with open(dir_path + '/model/setting/params.yml') as f:
@@ -34,24 +34,23 @@ tr.train_xgb(train_X, train_Y, params['xgb'])
 
 
 # 모델 저장
-# joblib.dump(tr.models_lgb, dir_path + '/model/data_model/lgb_model')
+joblib.dump(tr.models_lgb, dir_path + '/data/model/lgb_model')
 joblib.dump(tr.models_xgb, dir_path + '/data/model/xgb_model')
 
 # 예측
-# preds_ans1_lgb = pred.predict_lgb(tr.models_lgb, test1_X).sum()
-# preds_ans2_lgb = pred.predict_lgb(tr.models_lgb, test2_X).sum()
+preds_ans1_lgb = pred.predict_lgb(tr.models_lgb, test1_X).sum()
+preds_ans2_lgb = pred.predict_lgb(tr.models_lgb, test2_X).sum()
 
-# preds_ans1_xgb = pred.predict_xgb(tr.models_xgb, test1_X).sum()
-# preds_ans2_xgb = pred.predict_xgb(tr.models_xgb, test2_X).sum()
+preds_ans1_xgb = pred.predict_xgb(tr.models_xgb, test1_X).sum()
+preds_ans2_xgb = pred.predict_xgb(tr.models_xgb, test2_X).sum()
 
-# print('rmse of lgb:',tr.rmse_lgb)
-print('rmse of xgbr:',tr.rmse_xgbr)
+print('rmse of lgb:',tr.rmse_lgb)
 print('rmse of xgb:',tr.rmse_xgb)
 
 # 앙상블
-# preds_final_ans1 = preds_ans1_lgb * 0.5 + preds_ans1_xgb * 0.5
-# preds_final_ans2 = preds_ans2_lgb * 0.5 + preds_ans2_xgb * 0.5
+preds_final_ans1 = preds_ans1_lgb * 0.5 + preds_ans1_xgb * 0.5
+preds_final_ans2 = preds_ans2_lgb * 0.5 + preds_ans2_xgb * 0.5
 
 # 결과값 저장
-# submission['heat_supply_sum'] = np.array([preds_final_ans1,preds_final_ans2])
-# submission.to_csv(dir_path +'/submit/220156.csv', index=False)
+submission['heat_supply_sum'] = np.array([preds_final_ans1,preds_final_ans2])
+submission.to_csv(dir_path +'/submit/220156.csv', index=False)
